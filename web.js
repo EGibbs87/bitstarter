1f9c0bca70a71358cb014dba7ad5ed4d1a02fc97
Erik <flyers_rock_87@hotmail.com>
2013-07-18 01:34:45 +0000

    Update test file

--- web.js
+++ web.js
@@ -6,8 +6,10 @@ buf = new Buffer(fs.readFileSync('index.html','utf8'));
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+}
+);
